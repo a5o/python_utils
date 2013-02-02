130b95b3aedced7b3782ef9cb57a53201e4b3618
a5o <mistahkurtz2011@gmail.com>
2013-02-02 23:04:24 +0100

    Modificato email_image_creator.py
    Migliorata la gestione dei parametri di email_image_creator.py

--- email_image_creator.py
+++ email_image_creator.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
-import sys,Image,ImageFont,ImageDraw
+import argparse,Image,ImageFont,ImageDraw
 
-address=sys.argv[1]
-image = Image.new("RGBA", (len(address)*8,13), (255,255,255))
-usr_font = ImageFont.truetype("cour.ttf", 13)
-d_usr = ImageDraw.Draw(image)
-d_usr = d_usr.text((0,0), address,(0,0,0), font=usr_font)
-image.save(sys.argv[2])
+def main():
+        parser = argparse.ArgumentParser(description="Email image creator")
+        parser.add_argument('address',metavar="<email address>")
+        parser.add_argument('outfile',metavar="<output file>")
+        args = parser.parse_args()
+        address=args.address
+        image = Image.new("RGBA", (len(address)*8,13), (255,255,255))
+        usr_font = ImageFont.truetype("cour.ttf", 13)
+        d_usr = ImageDraw.Draw(image)
+        d_usr = d_usr.text((0,0), address,(0,0,0), font=usr_font)
+        image.save(args.outfile)
+
+if __name__ == "__main__":
+        main()
